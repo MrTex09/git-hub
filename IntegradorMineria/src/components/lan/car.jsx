9a7d81a0990f4b42c11298fc550f493d7f47f5d6
miqueas <bagente24@gmail.com>
2024-06-15 19:44:50 -0300

    se corrijio los estilos 3

--- IntegradorMineria/src/components/lan/car.jsx
+++ IntegradorMineria/src/components/lan/car.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 
+const carouselImageStyle = {
+  height: '650px',  // Alto de 50px
+  objectFit: 'cover', // Ajuste de la imagen para cubrir completamente el contenedor
+  margin: "50px"
+};
+
 export default function Car() {
   return (
-    <div className='car'>
-    <Carousel id="carouselExample" className="carousel slide">
-      <Carousel.Item className="carousel-item active">
-        <img src="images/logoGitHub.jpg" className="d-flex w-50 imagen" alt="Slide 1" />
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="First slide"
+          style={carouselImageStyle}  // Aplicar estilo aquí
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Second slide"
+          style={carouselImageStyle}  // Aplicar estilo aquí
+        />
+        <Carousel.Caption>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://via.placeholder.com/800x400"
+          alt="Third slide"
+          style={carouselImageStyle}  // Aplicar estilo aquí
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    </div>
-
   );
 }
